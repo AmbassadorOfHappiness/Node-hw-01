d6d4aec2957b1edb405ee4be4afea061bb214d75
AmbassadorOfHappiness <dmitrij.shevchuk@gmail.com>
2021-12-22 18:18:38 +0200

    Сhanged the path

--- controllers/contacts/getContactById.js
+++
@@ -1,9 +0,0 @@
-const listContacts = require('./listContacts');
-
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const [contact] = contacts.filter((contact) => String(contact.id) === String(contactId));
-  return contact;
-};
-
-module.exports = getContactById;
--- controllers/contactsPath.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-const contactsPath = path.resolve('db/contact.json');
-
-module.exports = contactsPath;
